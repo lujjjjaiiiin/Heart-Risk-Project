5541fe4de314cb596153b06a9764187d00f0dc4a
lujjjjaiiiin <134080698+lujjjjaiiiin@users.noreply.github.com>
2026-06-29 00:38:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+from xgboost import XGBClassifier
+
 
 # =========================
 # PAGE CONFIG
